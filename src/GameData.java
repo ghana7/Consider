a7cdb49c3e3c195d0b860206eb910c5b0fd9b0f2
Jimmie Harkins <ghana796@gmail.com>
2017-05-04 18:30:35 -0400

    added makeAspect method

--- src/GameData.java
+++ src/GameData.java
@@ -7,21 +7,27 @@ public class GameData {
 	public final static Map<String, Aspect> ASPECTDICT = new HashMap<String,Aspect>();
 	public final static Map<String, Item> ITEMDICT = new HashMap<String,Item>();
 	static {
-		ASPECTDICT.put("hardness", new Aspect("hardness", "Hardness"));
-		ASPECTDICT.put("heaviness", new Aspect("heaviness", "Heaviness"));
-		ASPECTDICT.put("smashing", new Aspect("smashing", "Smashing")); //hard + heavy
-		ASPECTDICT.put("crushing", new Aspect("crushing", "Crushing")); //smash + heavy
-		ASPECTDICT.get("smashing").setRecipe(new Aspect[] {ASPECTDICT.get("hardness"),ASPECTDICT.get("heaviness")});
-		ASPECTDICT.get("crushing").setRecipe(new Aspect[] {ASPECTDICT.get("smashing"),ASPECTDICT.get("heaviness")});
+		makeAspect("hardness","Hardness",null);
+		makeAspect("heaviness","Heaviness",null);
+		makeAspect("smashing","Smashing",new String[] {"hardness","heaviness"});
+		makeAspect("crushing","Crushing",new String[] {"smashing","heaviness"});
 	
 		ITEMDICT.put("rock", new Item("rock","Rock", new Aspect[] {a("heaviness"),a("crushing")}));
 		
 	}
 	
-	private static void makeAspect(String name, String display, String[] ingredients) {
-		
+	private static void makeAspect(String name, String display, String[] ingredients) { //shortcut for making a new aspect
+		ASPECTDICT.put(name, new Aspect(name,display));
+		if(ingredients != null) {
+			Aspect[] aspectRecipe = new Aspect[ingredients.length];
+			for(int i = 0; i < ingredients.length; i++) {
+				aspectRecipe[i] = a(ingredients[i]);
+			}
+			ASPECTDICT.get(name).setRecipe(aspectRecipe);
+		}
 	}
-	private static Aspect a(String s) {
+	
+	private static Aspect a(String s) { //just a shortcut for getting aspects from the dictionary
 		return ASPECTDICT.get(s);
 	}
 }
